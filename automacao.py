9178b0d85e577a1a5515dd851d3c09dd6b83b5cb
robertvoss <vossrobertt@gmail.com>
2022-07-12 20:35:32 -0300

    test working on google chrome using mac
    funcionou testando com googleshop e mudei a variavel padrao do chrome driver para o mac

--- automacao.py
+++ automacao.py
@@ -3,11 +3,10 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import pandas as pd
 from IPython.display import display
-import unittest
 
 
 #criando navegador
-nav = webdriver.Chrome()
+nav = webdriver.Chrome('/Users/robertvoss/Documents/GitHub/automacao-buscapreco/chromedriver')
 
 #importar/visualizar base de dados
 tabela_produtos = pd.read_excel("buscas.xlsx")
@@ -16,7 +15,15 @@ display(tabela_produtos)
 #entrar no google
 nav.get("https://www.google.com/")
 
-produto = 'iphone 12 64gb'
+produto = 'iphone 12 64 gb'
+produto = produto.lower()
+
+termos_banidos = 'mini watch'
+termos_banidos = termos_banidos.lower()
+lista_termos_banidos = termos_banidos.split(" ")
+lista_termos_produto = produto.split(" ")
+preco_minimo = '3000'
+preco_maximo = '3500'
 #pesquisar o nome do produto no google
 nav.find_element(By.XPATH,'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(produto)
 nav.find_element(By.XPATH,'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys(Keys.ENTER)
@@ -30,10 +37,38 @@ for item in elementos:
 lista_resultados = nav.find_elements(By.CLASS_NAME, 'sh-dgr__grid-result')
 
 for resultado in lista_resultados:
-    preco = resultado.find_elements(By.CLASS_NAME, 'T14wmb').text
-    nome = resultado.find_elements(By.CLASS_NAME, 'Xjkr3b').text
-    link = resultado.find_elements(By.CLASS_NAME, 'shntl').text
-    print(preco, nome, link)
+    nome = resultado.find_element(By.CLASS_NAME, 'Xjkr3b').text
+    nome = nome.lower()
+
+    #verificacao do nome
+    tem_termos_banidos = False
+    for palavra in lista_termos_banidos:
+        if palavra in nome:
+            tem_termos_banidos = True
+
+    tem_todos_termos_produto = True
+    for palavra in lista_termos_produto:
+        if palavra not in nome:
+            tem_todos_termos_produto = False
+
+    # se tem_termos_banidos = False e o tem_todos_termos_produto=True entao
+    if not tem_termos_banidos and tem_todos_termos_produto: #verifica nome
+        preco = resultado.find_element(By.CLASS_NAME, 'a8Pemb').text
+        preco = preco.replace("R$", "").replace(" ", "").replace(".", "").replace(",", ".")
+        preco = float(preco)
+
+    # verificando se preco ta minimo ou maximo
+        preco_maximo = float(preco_maximo)
+        preco_minimo = float(preco_minimo)
+        if preco_minimo <= preco <= preco_maximo:
+            elemento_link = resultado.find_element(By.CLASS_NAME, 'aULzUe')
+            elemento_pai = elemento_link.find_element(By.XPATH, '..')
+            link = elemento_pai.get_attribute('href')
+            print(preco, nome, link)
+
+
+
+
 
 #para cada item na base de dados ->
     #procurar produto no google shopping
